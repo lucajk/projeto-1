8231582602943aee2631576e22069620f0008540
lucajk <lucajk95@gmail.com>
2024-10-01 20:04:31 +0000

    mudança de vg

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h> // Para função sleep
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
-}
+// Variáveis globais
+double saldo_reais = 0, saldo_bitcoin = 0, saldo_ethereum = 0, saldo_ripple = 0;
+double BTC = 500, ETH = 200, XRP = 100;
+double variacao_bitcoin, variacao_ripple, variacao_ethereum;
+char extrato[1000] = "";
+char Nome[] = "Luca Juraski";
+char login[12], senha[12];
+
